c0ad1e60c00f23d9b1e9af31d94727fa87995cf8
Sinzxc <sin.zxc@bk.ru>
2023-01-21 18:55:00 +0300

    second

--- GitTEst/Form1.cs
+++ GitTEst/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTEst
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString());
+        }
     }
 }
