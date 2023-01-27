580b09b8efa8247cba3f88d66d5d78c3f0fe17d9
Sinzxc <sin.zxc@bk.ru>
2023-01-27 19:24:16 +0300

    Exeption tests

--- GitTEst/Form1.cs
+++ GitTEst/Form1.cs
@@ -16,10 +16,32 @@ namespace GitTEst
         {
             InitializeComponent();
         }
-
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException();
+            return x / y;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            double a = 98, b = 0;
+            double result;
+            int [] A = new int[5];
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+                A[6] = 1;
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Attempted divide by zero.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
             MessageBox.Show(DateTime.Now.ToString());
         }
     }
-}
+}   
